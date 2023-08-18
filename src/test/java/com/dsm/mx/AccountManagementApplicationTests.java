c4f3763588033377279b7c76f12f21ea0342a791
Francisco Jimenez <fran@192.168.1.6>
2023-08-17 22:13:40 -0600

    Initial commit

---
+++ src/test/java/com/dsm/mx/AccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsm.mx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
