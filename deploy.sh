e497242be4928627c3b24c3a68d4412881e0ec88
Francisco Jimenez <fran@192.168.1.7>
2023-09-27 18:04:48 -0600

    Initial deployment script

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+docker pull dendral/sremx:0.2.2
+#sshPublisher(publishers: [sshPublisherDesc(configName: 'remotedocker', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'deploy.sh', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '/home/dockeradmin/jenkins', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '*.sh')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
