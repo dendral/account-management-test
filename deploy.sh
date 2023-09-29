de0ffb1c8f38148377b714b665b3a97f5f140688
Francisco Jimenez <fran@192.168.1.7>
2023-09-28 23:01:38 -0600

    Refresh image on reinstall

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
-docker pull dendral/sremx:0.0.0
-docker run -p 9090:8080 dendral/sremx:0.0.0
+docker rmi -f dendral/sremx:0.0.0
+docker rm -f account-service
+docker run --name account-service -d -p 9090:8080 dendral/sremx:0.0.0
