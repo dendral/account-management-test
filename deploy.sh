3466c2d0e821579fd99793f6d19d6556266994bb
Francisco Jimenez <fran@192.168.1.7>
2023-09-30 00:55:22 -0600

    Update docker image in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker rmi -f dendral/account-management:0.0.0
 docker rm -f account-management
-docker run --name account-management -d -p 9090:8080 account-management:0.0.0
+docker run --name account-management -d -p 9090:8080 dendral/account-management:0.0.0
